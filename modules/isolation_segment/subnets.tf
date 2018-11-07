24743ced380caa03ad4006f2543d68887ac2492b
Mark Alston <malston@pivotal.io>
2018-11-07 10:11:23 -0700

    Use azurerm_subnet_network_security_group_association to assign nsgs to subnets

--- modules/isolation_segment/subnets.tf
+++ modules/isolation_segment/subnets.tf
@@ -5,5 +5,11 @@ resource "azurerm_subnet" "iso_seg_subnet" {
   resource_group_name       = "${var.resource_group_name}"
   virtual_network_name      = "${var.network_name}"
   address_prefix            = "${element(var.iso_seg_subnet_blocks, count.index)}"
+}
+
+resource "azurerm_subnet_network_security_group_association" "iso_seg_subnet_network_security_group_association" {
+  count = "${var.count}"
+
+  subnet_id                 = "${azurerm_subnet.iso_seg_subnet.*.id[count.index]}"
   network_security_group_id = "${var.bosh_deployed_vms_security_group_id}"
 }
